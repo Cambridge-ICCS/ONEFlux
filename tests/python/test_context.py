c290912316692dda01c4f3ea5164336308b7994a
Matthew Archer <ma595@cam.ac.uk>
2023-04-04 10:50:47 +0100

    test_context.py now converted to pytest

--- tests/python/test_context.py
+++ tests/python/test_context.py
@@ -8,14 +8,7 @@ Simple context/import setup test
 @contact: gzpastorello@lbl.gov
 @date: 2017-01-31
 '''
-import unittest
 
-from context import oneflux
-
-class BasicTest(unittest.TestCase):
-    def test_context(self):
-        """Test import by checking imported 'oneflux' module has '__version__' attribute"""
-        self.assertTrue(hasattr(oneflux, '__version__'))
-
-if __name__ == '__main__':
-    unittest.main()
+def test_import_oneflux():
+    import oneflux
+    assert hasattr(oneflux, '__version__') == True
