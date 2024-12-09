f9299fe6823305b571313dc25342328a9235c14d
James Emberton <jamie.emberton@gmail.com>
2024-12-09 10:38:20 +0000

    hook

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
 """
 This module contains pytest fixtures and utility functions to set up the test environment,
-handle MATLAB engine interactions, and process text files for comparison in unit tests. 
+handle MATLAB engine interactions, and process text files for comparison in unit tests.
 
 Contents:
     Fixtures:
