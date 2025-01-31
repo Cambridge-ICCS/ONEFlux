60622937b6a952f10f7a2e9dd770086a56cf7f00
Dominic Orchard <dom.orchard@gmail.com>
2025-01-31 22:29:38 +0000

    comment on differential test

--- tests/unit_tests/test_ustar_cp/test_fcDatevec.py
+++ tests/unit_tests/test_ustar_cp/test_fcDatevec.py
@@ -84,7 +84,7 @@ def test_fcDatevec_specific(test_engine, t, expected):
     # Check the result
     assert test_engine.equal(result, test_engine.convert(expected))
 
-
+# TODO: During migration remove this differential test
 def test_fcDatevec_site_data(test_engine):
 
     time_artifact_path = 'tests/test_artifacts/cpdEvaluateUStarTh4Season20100901_artifacts/CA-Cbo_qca_ustar_2007/input_time_it_.csv'
